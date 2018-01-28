4fbf4e869edb0b7ad9f6169aed6a924b798e92c5
Tyler Audette <tyleraudette5@gmail.com>
2018-01-27 18:24:09 -0700

    Outline actions & reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,45 @@
-export const testAction = (data) => ({
-  type: 'TEST_ACTION',
-  data: data
-});
+import { FETCHING_VIDEOS, FETCHING_VIDEOS_SUCCESS, FETCHING_VIDEOS_FAILURE } from './actionTypes';
+
+const url = 'https://d6api.gaia.com/videos/term/119931'
+const headers = { 'Content-Type': 'application/json' } 
+
+export const getVideos = () => {
+  return {
+    type: FETCHING_VIDEOS
+  }
+}
+
+export const getVideosSuccess = (data) => {
+  return {
+    type: FETCHING_VIDEOS_SUCCESS,
+    data
+  }
+}
+
+export const getVideosFailure = () => {
+  return {
+    type: FETCHING_VIDEOS_FAILURE
+  }
+}
+
+export const fetchVideos = () => {
+  return (dispatch) => {
+    //set loader
+    dispatch(getVideos())
+
+    fetch(url, headers)
+      .then((resp) => {
+        if(!resp.ok) {
+          throw Error(response.statusText)
+        }
+        return resp
+      })
+      .then((res) => response.json())
+      .then((data) => { 
+        dispatch(getVideosSuccess(data))
+      })
+      .catch((error) => {
+        dispatch(getVideosFailure)
+      })
+  }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,32 @@
-const initialState = { data: '' };
+import { FETCHING_VIDEOS, FETCHING_VIDEOS_SUCCESS, FETCHING_VIDEOS_FAILURE } from '../actions/actionTypes'
 
-export default (state = initialState, action) => {
+const initialState = { 
+  videos: [],
+  videosFetched: false,
+  isFetching: false,
+  error: false
+}
+
+export default function reducer (state = initialState, action) {
   switch (action.type) {
-    case 'TEST_REDUCER':
-    return {
-      ...state,
-      data: 'testData'
-    }
+    case 'FETCHING_VIDEOS':
+      return {
+        ...state,
+        videos: [],
+        isFetching: true
+      }
+    case 'FETCHING_VIDEOS_SUCCESS':
+      return {
+        ...state,
+        isFetching: false,
+        videos: action.data
+      }
+    case 'FETCHING_VIDEOS_FAILURE':
+      return {
+        ...state,
+        isFetching: false,
+        error: true
+      }
     default:
       return state
   }
