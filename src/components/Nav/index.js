413807385a97489773e8ae180604ac7565f95ea6
Tyler Audette <tyleraudette5@gmail.com>
2018-01-28 21:01:09 -0700

    Add bootstrap

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Navbar, Nav, NavItem, FormGroup, FormControl, Button } from 'react-bootstrap';
+import styles from '../../styles/components/_nav.scss'
 
 export const NavComponent = () => (
-  <div>
-    Nav Component
-  </div>
+  <Navbar className={styles.nav} collapseOnSelect>
+    <Navbar.Toggle />
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem >MY GAIA</NavItem>
+        <NavItem>YOGA</NavItem>
+        <NavItem>SEEKING TRUTH</NavItem>
+        <NavItem>TRANSFORMATION</NavItem>
+        <NavItem>FILMS & DOCS</NavItem>
+        <NavItem>CENTERS</NavItem>
+      </Nav>
+      <Navbar.Form pullRight>
+        <FormGroup>
+          <FormControl type="text" placeholder="Search" />
+        </FormGroup>{' '}
+        <Button className={styles.nav} type="submit">Submit</Button>
+      </Navbar.Form>
+    </Navbar.Collapse>
+  </Navbar>
+
 )
 
 export default NavComponent
