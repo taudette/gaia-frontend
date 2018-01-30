5a4b5db12f990d7814c9f124158996fc08b1bc7e
Tyler Audette <tyleraudette5@gmail.com>
2018-01-29 17:19:33 -0700

    Add dropdown and update styling

--- src/components/Videos/index.js
+++ src/components/Videos/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import VideoComponent from '../Video'
+import VideoComponent from '../Video';
+import { DropdownButton, MenuItem, Row, Col } from 'react-bootstrap';
 import styles from '../../styles/components/_videos.scss';
 
 export const VideosComponent = (props) => {
@@ -8,15 +9,28 @@ export const VideosComponent = (props) => {
   }
 
   return (
-    <div >
-      {props.videos &&
-        <ul className={styles.videoList}>
-          {props.videos.map((video, index) => {
-            return <VideoComponent {...video} key={index} />
-          })}
-        </ul>
-      }
-    </div>
+    <section >
+      <div className={styles.videoList} >
+        <Row>
+          <Col lg={3} lgOffset={9} md={4} mdOffset={8} sm={6} smOffset={6} xs={12} key={props.index} className={`${styles.buttonCol} hidden-xs`}>
+            <p>SORT BY</p>
+            <DropdownButton bsStyle='default' className={styles.dropDownButton} title='Choose an Option' id='sort videos'>
+              <MenuItem eventKey="1">Name</MenuItem>
+              <MenuItem eventKey="2">Date</MenuItem>
+              <MenuItem eventKey="2">Category</MenuItem>
+            </DropdownButton>
+          </Col>
+        </Row>
+        <p>We suggest you start here &#x2192;</p>
+        {props.videos &&
+          <ul >
+            {props.videos.map((video, index) => {
+              return <VideoComponent {...video} key={index} />
+            })}
+          </ul>
+        }
+      </div>
+    </section>
   )
 }
 
